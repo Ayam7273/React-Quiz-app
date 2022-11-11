13d7bb814772c7b46871046c0dda040238c9f39a
ABDULBASIT ADIO <abdulbasitadio@gmail.com>
2022-11-11 01:05:37 +0100

    quiz app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage'
+import QuizPage from './pages/QuizPage'
+import CreateQuizPage from './pages/CreateQuizPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Routes>
+       <Route path='/' element={<HomePage />} />
+       <Route path='/quiz' element={<QuizPage />} />
+       <Route path='/create' element={<CreateQuizPage />} />
+       <Route path='/quiz' element={<QuizPage />} />
+       <Route path='*' element={<NotFoundPage />} />
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
